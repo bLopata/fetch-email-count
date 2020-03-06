6f01ffb5caf85c6bdd3d442580152553f3fd338f
Ben Lopata <benlopata@gmail.com>
2020-03-05 23:00:03 -0600

    Cleaned up formatting.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var polka = require('polka')
-var { json } = require('body-parser')
+var polka = require("polka");
+var { json } = require("body-parser");
 
 polka()
-    .use(json())
-    .post('/emails', (req, res) => {
-      res.writeHead(200, { 'Content-Type': 'application/json' })
-      let json = JSON.stringify(countEmails(req.body))
-      res.end(json)
+  .use(json())
+  .post("/emails", (req, res) => {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    let json = JSON.stringify(countEmails(req.body));
+    res.end(json);
   })
   .listen(3000, err => {
     if (err) throw err;
@@ -14,15 +14,15 @@ polka()
   });
 
 function countEmails(emailList) {
-    var counts = {};
-    for(let email of emailList){
-      while(email.indexOf(".") < email.indexOf("@")){
-        email = email.replace(".", "")
-      }
-      if(email.includes("+")){
-        email = email.replace(/(\+[a-z]{0,})/gi, "")
-      }
-      counts[email] = 1 + (counts[email] || 0);
+  var counts = {};
+  for (let email of emailList) {
+    while (email.indexOf(".") < email.indexOf("@")) {
+      email = email.replace(".", "");
     }
-		return Object.keys(counts).length
+    if (email.includes("+")) {
+      email = email.replace(/(\+[a-z]{0,})/gi, "");
+    }
+    counts[email] = 1 + (counts[email] || 0);
+  }
+  return Object.keys(counts).length;
 }
